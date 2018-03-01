f7208005e7f23c9cf3216a6a34ec12d70faa41c0
ani ruhil <ruhil@ohio.edu>
2018-02-28 22:27:00 -0500

    package first commit

---
+++ data-raw/data_prep.R
@@ -0,0 +1,116 @@
+library(gutenbergr)
+
+mymanjeeves <- gutenberg_download(8164)$text
+devtools::use_data(mymanjeeves, overwrite = TRUE)
+
+righthojeeves <- gutenberg_download(10554)$text
+devtools::use_data(righthojeeves, overwrite = TRUE)
+
+themanwithtwoleftfeet <- gutenberg_download(7471)$text
+devtools::use_data(themanwithtwoleftfeet, overwrite = TRUE)
+
+somethingnew <- gutenberg_download(2042)$text
+devtools::use_data(somethingnew, overwrite = TRUE)
+
+themanupstairs <- gutenberg_download(6768)$text
+devtools::use_data(themanupstairs, overwrite = TRUE)
+
+psmithinthecity <- gutenberg_download(6753)$text
+devtools::use_data(psmithinthecity, overwrite = TRUE)
+
+thegirlontheboat <- gutenberg_download(20717)$text
+devtools::use_data(thegirlontheboat, overwrite = TRUE)
+
+adamselindistress <- gutenberg_download(2233)$text
+devtools::use_data(adamselindistress, overwrite = TRUE)
+
+theadventuresofsally <- gutenberg_download(7464)$text
+devtools::use_data(theadventuresofsally, overwrite = TRUE)
+
+picadillyjim <- gutenberg_download(2005)$text
+devtools::use_data(picadillyjim, overwrite = TRUE)
+
+awodehousemiscellany <- gutenberg_download(8190)$text
+devtools::use_data(awodehousemiscellany, overwrite = TRUE)
+
+psmithjournalist <- gutenberg_download(2607)$text
+devtools::use_data(psmithjournalist, overwrite = TRUE)
+
+theclickingofcuthbert <- gutenberg_download(7028)$text
+devtools::use_data(theclickingofcuthbert, overwrite = TRUE)
+
+uneasymoney <- gutenberg_download(6684)$text
+devtools::use_data(uneasymoney, overwrite = TRUE)
+
+mikeandpsmith <- gutenberg_download(10586)$text
+devtools::use_data(mikeandpsmith, overwrite = TRUE)
+
+deathattheexcelsioandotherstories <- gutenberg_download(8176)$text
+devtools::use_data(deathattheexcelsioandotherstories, overwrite = TRUE)
+
+indiscretionsofarchie <- gutenberg_download(3756)$text
+devtools::use_data(indiscretionsofarchie, overwrite = TRUE)
+
+loveamongthechikens <- gutenberg_download(3829)$text
+devtools::use_data(loveamongthechikens, overwrite = TRUE)
+
+mike <- gutenberg_download(7423)$text
+
+thepolitenessofprincesandotherstories <- gutenberg_download(8178)$text
+devtools::use_data(thepolitenessofprincesandotherstories, overwrite = TRUE)
+
+amanofmeans <- gutenberg_download(8713)$text
+devtools::use_data(amanofmeans, overwrite = TRUE)
+
+theintrusionofjimmy <- gutenberg_download(4075)$text
+devtools::use_data(theintrusionofjimmy, overwrite = TRUE)
+
+jillthereckless <- gutenberg_download(20533)$text
+devtools::use_data(jillthereckless, overwrite = TRUE)
+
+theswooporhowclarencesavedengland <- gutenberg_download(7050)$text
+devtools::use_data(theswooporhowclarencesavedengland, overwrite = TRUE)
+
+threemenandamaid <- gutenberg_download(6836)$text
+devtools::use_data(threemenandamaid, overwrite = TRUE)
+
+thepothunters <- gutenberg_download(6984)$text
+devtools::use_data(thepothunters, overwrite = TRUE)
+
+thewhitefeather <- gutenberg_download(6927)$text
+devtools::use_data(thewhitefeather, overwrite = TRUE)
+
+aprefectsuncle <- gutenberg_download(6985)$text
+devtools::use_data(aprefectsuncle, overwrite = TRUE)
+
+theheadofkays <- gutenberg_download(6877)$text
+devtools::use_data(theheadofkays, overwrite = TRUE)
+
+talesofstaustins <- gutenberg_download(6980)$text
+devtools::use_data(talesofstaustins, overwrite = TRUE)
+
+thegoldbat <- gutenberg_download(6879)$text
+devtools::use_data(thegoldbat, overwrite = TRUE)
+
+williamtelltoldagain <- gutenberg_download(7298)$text
+devtools::use_data(williamtelltoldagain, overwrite = TRUE)
+
+theprinceandbetty <- gutenberg_download(6955)$text
+devtools::use_data(theprinceandbetty, overwrite = TRUE)
+
+thelittlenugget <- gutenberg_download(6683)$text
+devtools::use_data(thelittlenugget, overwrite = TRUE)
+
+thegemcollector <- gutenberg_download(8931)$text
+devtools::use_data(thegemcollector, overwrite = TRUE)
+
+thecomingofbill  <- gutenberg_download(6880)$text
+devtools::use_data(thecomingofbill, overwrite = TRUE)
+
+thelittlewarrior <- gutenberg_download(6837)$text
+devtools::use_data(thelittlewarrior, overwrite = TRUE)
+
+notgeorgewashington <- gutenberg_download(7230)$text
+devtools::use_data(notgeorgewashington, overwrite = TRUE)
+
+
