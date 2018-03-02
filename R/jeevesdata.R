8890aaf64a18154e20aba1f4a8f76d86453fe915
ani ruhil <ruhil@ohio.edu>
2018-03-02 08:33:15 -0500

    package man

---
+++ R/jeevesdata.R
@@ -0,0 +1,364 @@
+#' Selected novels of P. G. Wodehouse
+#'
+#' @description This package contains the complete text of selected novels of
+#' P. G. Wodehouse formatted to be convenient for text analysis.
+#' @docType package
+#' @name jeeves
+#' @aliases wodehouse jeeves pgwodehouse psmith
+NULL
+
+
+#' "A Damsel in Distress"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' novel "A Damsel in Distress". First published in 1919, the plot revolves
+#' around golf-loving #' American composer George Bevan who falls in love with
+#' a mysterious young lady who takes refuge in his taxicab one day. When he
+#' later tracks her down to a romantic rural manor, mistaken identity leads to
+#'all manner of brouhaha.
+#'
+#' @source \url{https://www.gutenberg.org/files/2233/2233-8.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"adamselindistress"
+
+#' "A Man of Means"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' "A Man of Means", a collection of six short stories published in 1914,
+#' starring Roland Beake, whose financial success is not always a blessing.
+#'
+#' @source \url{http://www.gutenberg.org/files/8713/8713.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"amanofmeans"
+
+#' "A Prefect's Uncle"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' "A Prefect's Uncle", a collection of stories set in a private school for boys.
+#'
+#' @source \url{http://www.gutenberg.org/files/6985/6985.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"aprefectsuncle"
+
+#' "A Wodehouse Miscellany"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' "A Wodehouse Miscellany", a collection of articles, poems, and stories.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/8190/pg8190.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"awodehousemiscellany"
+
+#' "Death at The Excelsior and Other Stories"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#'"Death at the Excelsior and Other Stories", a postumously published collection of short
+#' stories.
+#'
+#' #' @source \url{http://www.gutenberg.org/cache/epub/8176/pg8176.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"deathattheexcelsiorandotherstories"
+
+#' "Indiscretions of Archie"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' "Indiscretions of Archie", a hilarious narrative documenting Archie's travails in
+#' America, chasing love.
+#'
+#' #' @source \url{http://www.gutenberg.org/files/3756/3756-0.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"indiscretionsofarchie"
+
+#' "Jill the Reckless"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' 1920 classic love story that spans the Atlantic.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/20533/pg20533.txt}
+#' #' @format A `tbl_df`, `tbl`, and `data.frame`
+"jillthereckless"
+
+#' "Love Among the Chickens"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' "Love Among the Chickens", a book that launched his career.
+#' @source \url{http://www.gutenberg.org/cache/epub/3829/pg3829.txt}
+#' #' @format A `tbl_df`, `tbl`, and `data.frame`
+"loveamongthechickens"
+
+#' "Mike"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' "Mike", the work that introduced us to Mike (and a whole lot of cricketing jargon).
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/7423/pg7423.txt}
+#' #' @format A `tbl_df`, `tbl`, and `data.frame`
+"mike"
+
+#' "Mike and Psmith"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' "Mike and Psmith", a story  about two friends at a boarding school.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/10586/pg10586.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"mikeandpsmith"
+
+#' "My Man Jeeves"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's 1919
+#' "My Man Jeeves", a collection of short stories that feature Jeeves,
+#' Bertie Wooster, and Reggie Pepper (an early prototype for Bertie Wooster).
+#'
+#' @source \url{https://www.gutenberg.org/files/8164/8164.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"mymanjeeves"
+
+#' "Not George Washington: A Smi-autobiographical Novel"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#'semi-autobiographical novel.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/7230/pg7230.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"notgeorgewashington"
+
+#' "Piccadilly Jim"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' rollicking collection of four novels.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/2005/pg2005.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"piccadillyjim"
+
+#' "Psmith in the City"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' narrative revolving around Mike Jackson, Psmith, and cricketing dreams dashed by fate, or are they?
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/6753/pg6753.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"psmithinthecity"
+
+#' "Psmith, Journalist"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' contiuing saga of Mike and Psmith and more cricket.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/2607/pg2607.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"psmithjournalist"
+
+#' "Right Ho, Jeeves"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' classic collection of tales revolving around Bertie Wooster and the unflappable Jeeves.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/10554/pg10554.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"righthojeeves"
+
+#' "Something New"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' hilarious saga of goings on at Blandings Castle.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/2042/pg2042.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"somethingnew"
+
+#' "Tales of St. Austins"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' collection of short stories and essays set in a public school, revolving around rugby
+#' and cricket.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/6980/pg6980.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"talesofstaustins"
+
+#' "The Adventures of Sally"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' novel about the travails and tribulations of a heiress.
+#'
+#' @source \url{http://www.gutenberg.org/files/7464/7464-0.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"theadventuresofsally"
+
+#' "The Clicking of Cuthbert"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' classic fare that involves golf, and more golf.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/7028/pg7028.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"theclickingofcuthbert"
+
+#' "The Coming of Bill"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' "serious" novel.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/6880/pg6880.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"thecomingofbill"
+
+#' "The Gem Collector"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' tale of Spike's misdeeds and eleventh-hour rescues that set the world right.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/8931/pg8931.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"thegemcollector"
+
+#' "The Girl on the Boat"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' novek about red-haired, dog-loving "Billie".
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/20717/pg20717.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"thegirlontheboat"
+
+#' "The Gold Bat"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' novel set in Wrykyn School.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/20717/pg20717.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"thegoldbat"
+
+#' "The Head of Kays"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' novel set in a public school beset with the usual panoply of unruly pupils.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/6877/pg6877.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"theheadofkays"
+
+#' "The Intrusion of Jimmy"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' masterful treatment of romantic farce.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/4075/pg4075.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"theintrusionofjimmy"
+
+#' "The Little Nugget"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' 1913 coming-of-age light comic novel set in a prep school.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/6683/pg6683.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"thelittlenugget"
+
+#' "The Little Warrior"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' romantic tale set on both sides of the Atlantic with uncommon twists and turns.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/6837/pg6837.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"thelittlewarrior"
+
+#' "The Man Upstairs and Other Stories"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' "The Man Upstairs"
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/6768/pg6768.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"themanupstairs"
+
+#' "The Man with Two Left Feet"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' collection of short stories that might introduce Jeeves.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/7471/pg7471.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"themanwithtwoleftfeet"
+
+#' "The Politeness of Princes and Other School Stories"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' short story published in 1902.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/8178/pg8178.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"thepolitenessofprincesandotherstories"
+
+#'  "The Pothunters"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' novel about stolen silver cups from a cricket pavillion.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/6984/pg6984.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"thepothunters"
+
+#' "The Prince and Betty"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' novel about a scheming billionaire, his daughter's love interest, and casino in the works.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/6955/pg6955.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"theprinceandbetty"
+
+#' "The Swoop or how Clarence Saved England"
+#'
+#'@description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' "The Swoop!, or How Clarence Saved England".
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/7050/pg7050.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"theswooporhowclarencesavedengland"
+
+#' "The White Feather"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' novel set in Wrykyn School.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/6927/pg6927.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"thewhitefeather"
+
+#' "Three Men and a Maid"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' "A Girl on a Boat", initially published in the United States as "Three Men and a Maid".
+#' I have kept it in the database because the length seems to be different from that of
+#' "A Girl on a Boat" and I'd be curious to see why.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/6836/pg6836.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"threemenandamaid"
+
+#' "Uneasy Money"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' novel about a man finding it hard to give money away to those hard-done by a will,
+#' and is set in Long Island (NY).
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/6684/pg6684.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"uneasymoney"
+
+#' "William Tell Told Again"
+#'
+#' @description A data set containing the complete text of Pelham Grenville Wodehouse's
+#' retelling of the legendary tales of William Tell, in prose and verse.
+#'
+#' @source \url{http://www.gutenberg.org/cache/epub/7298/pg7298.txt}
+#' @format A `tbl_df`, `tbl`, and `data.frame`
+"williamtelltoldagain"
+
+
--- data-raw/data_prep.R
+++ data-raw/data_prep.R
@@ -1,116 +1,117 @@
 library(gutenbergr)
 
-mymanjeeves <- gutenberg_download(8164)$text
+mymanjeeves <- gutenberg_download(8164, meta_fields = "title")
 devtools::use_data(mymanjeeves, overwrite = TRUE)
 
-righthojeeves <- gutenberg_download(10554)$text
+righthojeeves <- gutenberg_download(10554, meta_fields = "title")
 devtools::use_data(righthojeeves, overwrite = TRUE)
 
-themanwithtwoleftfeet <- gutenberg_download(7471)$text
+themanwithtwoleftfeet <- gutenberg_download(7471, meta_fields = "title")
 devtools::use_data(themanwithtwoleftfeet, overwrite = TRUE)
 
-somethingnew <- gutenberg_download(2042)$text
+somethingnew <- gutenberg_download(2042, meta_fields = "title")
 devtools::use_data(somethingnew, overwrite = TRUE)
 
-themanupstairs <- gutenberg_download(6768)$text
+themanupstairs <- gutenberg_download(6768, meta_fields = "title")
 devtools::use_data(themanupstairs, overwrite = TRUE)
 
-psmithinthecity <- gutenberg_download(6753)$text
+psmithinthecity <- gutenberg_download(6753, meta_fields = "title")
 devtools::use_data(psmithinthecity, overwrite = TRUE)
 
-thegirlontheboat <- gutenberg_download(20717)$text
+thegirlontheboat <- gutenberg_download(20717, meta_fields = "title")
 devtools::use_data(thegirlontheboat, overwrite = TRUE)
 
-adamselindistress <- gutenberg_download(2233)$text
+adamselindistress <- gutenberg_download(2233, meta_fields = "title")
 devtools::use_data(adamselindistress, overwrite = TRUE)
 
-theadventuresofsally <- gutenberg_download(7464)$text
+theadventuresofsally <- gutenberg_download(7464, meta_fields = "title")
 devtools::use_data(theadventuresofsally, overwrite = TRUE)
 
-picadillyjim <- gutenberg_download(2005)$text
-devtools::use_data(picadillyjim, overwrite = TRUE)
+piccadillyjim <- gutenberg_download(2005, meta_fields = "title")
+devtools::use_data(piccadillyjim, overwrite = TRUE)
 
-awodehousemiscellany <- gutenberg_download(8190)$text
+awodehousemiscellany <- gutenberg_download(8190, meta_fields = "title")
 devtools::use_data(awodehousemiscellany, overwrite = TRUE)
 
-psmithjournalist <- gutenberg_download(2607)$text
+psmithjournalist <- gutenberg_download(2607, meta_fields = "title")
 devtools::use_data(psmithjournalist, overwrite = TRUE)
 
-theclickingofcuthbert <- gutenberg_download(7028)$text
+theclickingofcuthbert <- gutenberg_download(7028, meta_fields = "title")
 devtools::use_data(theclickingofcuthbert, overwrite = TRUE)
 
-uneasymoney <- gutenberg_download(6684)$text
+uneasymoney <- gutenberg_download(6684, meta_fields = "title")
 devtools::use_data(uneasymoney, overwrite = TRUE)
 
-mikeandpsmith <- gutenberg_download(10586)$text
+mikeandpsmith <- gutenberg_download(10586, meta_fields = "title")
 devtools::use_data(mikeandpsmith, overwrite = TRUE)
 
-deathattheexcelsioandotherstories <- gutenberg_download(8176)$text
-devtools::use_data(deathattheexcelsioandotherstories, overwrite = TRUE)
+deathattheexcelsiorandotherstories <- gutenberg_download(8176, meta_fields = "title")
+devtools::use_data(deathattheexcelsiorandotherstories, overwrite = TRUE)
 
-indiscretionsofarchie <- gutenberg_download(3756)$text
+indiscretionsofarchie <- gutenberg_download(3756, meta_fields = "title")
 devtools::use_data(indiscretionsofarchie, overwrite = TRUE)
 
-loveamongthechikens <- gutenberg_download(3829)$text
-devtools::use_data(loveamongthechikens, overwrite = TRUE)
+loveamongthechickens <- gutenberg_download(3829, meta_fields = "title")
+devtools::use_data(loveamongthechickens, overwrite = TRUE)
 
-mike <- gutenberg_download(7423)$text
+mike <- gutenberg_download(7423, meta_fields = "title")
+devtools::use_data(mike, overwrite = TRUE)
 
-thepolitenessofprincesandotherstories <- gutenberg_download(8178)$text
+thepolitenessofprincesandotherstories <- gutenberg_download(8178, meta_fields = "title")
 devtools::use_data(thepolitenessofprincesandotherstories, overwrite = TRUE)
 
-amanofmeans <- gutenberg_download(8713)$text
+amanofmeans <- gutenberg_download(8713, meta_fields = "title")
 devtools::use_data(amanofmeans, overwrite = TRUE)
 
-theintrusionofjimmy <- gutenberg_download(4075)$text
+theintrusionofjimmy <- gutenberg_download(4075, meta_fields = "title")
 devtools::use_data(theintrusionofjimmy, overwrite = TRUE)
 
-jillthereckless <- gutenberg_download(20533)$text
+jillthereckless <- gutenberg_download(20533, meta_fields = "title")
 devtools::use_data(jillthereckless, overwrite = TRUE)
 
-theswooporhowclarencesavedengland <- gutenberg_download(7050)$text
+theswooporhowclarencesavedengland <- gutenberg_download(7050, meta_fields = "title")
 devtools::use_data(theswooporhowclarencesavedengland, overwrite = TRUE)
 
-threemenandamaid <- gutenberg_download(6836)$text
+threemenandamaid <- gutenberg_download(6836, meta_fields = "title")
 devtools::use_data(threemenandamaid, overwrite = TRUE)
 
-thepothunters <- gutenberg_download(6984)$text
+thepothunters <- gutenberg_download(6984, meta_fields = "title")
 devtools::use_data(thepothunters, overwrite = TRUE)
 
-thewhitefeather <- gutenberg_download(6927)$text
+thewhitefeather <- gutenberg_download(6927, meta_fields = "title")
 devtools::use_data(thewhitefeather, overwrite = TRUE)
 
-aprefectsuncle <- gutenberg_download(6985)$text
+aprefectsuncle <- gutenberg_download(6985, meta_fields = "title")
 devtools::use_data(aprefectsuncle, overwrite = TRUE)
 
-theheadofkays <- gutenberg_download(6877)$text
+theheadofkays <- gutenberg_download(6877, meta_fields = "title")
 devtools::use_data(theheadofkays, overwrite = TRUE)
 
-talesofstaustins <- gutenberg_download(6980)$text
+talesofstaustins <- gutenberg_download(6980, meta_fields = "title")
 devtools::use_data(talesofstaustins, overwrite = TRUE)
 
-thegoldbat <- gutenberg_download(6879)$text
+thegoldbat <- gutenberg_download(6879, meta_fields = "title")
 devtools::use_data(thegoldbat, overwrite = TRUE)
 
-williamtelltoldagain <- gutenberg_download(7298)$text
+williamtelltoldagain <- gutenberg_download(7298, meta_fields = "title")
 devtools::use_data(williamtelltoldagain, overwrite = TRUE)
 
-theprinceandbetty <- gutenberg_download(6955)$text
+theprinceandbetty <- gutenberg_download(6955, meta_fields = "title")
 devtools::use_data(theprinceandbetty, overwrite = TRUE)
 
-thelittlenugget <- gutenberg_download(6683)$text
+thelittlenugget <- gutenberg_download(6683, meta_fields = "title")
 devtools::use_data(thelittlenugget, overwrite = TRUE)
 
-thegemcollector <- gutenberg_download(8931)$text
+thegemcollector <- gutenberg_download(8931, meta_fields = "title")
 devtools::use_data(thegemcollector, overwrite = TRUE)
 
-thecomingofbill  <- gutenberg_download(6880)$text
+thecomingofbill  <- gutenberg_download(6880, meta_fields = "title")
 devtools::use_data(thecomingofbill, overwrite = TRUE)
 
-thelittlewarrior <- gutenberg_download(6837)$text
+thelittlewarrior <- gutenberg_download(6837, meta_fields = "title")
 devtools::use_data(thelittlewarrior, overwrite = TRUE)
 
-notgeorgewashington <- gutenberg_download(7230)$text
+notgeorgewashington <- gutenberg_download(7230, meta_fields = "title")
 devtools::use_data(notgeorgewashington, overwrite = TRUE)
 
 
